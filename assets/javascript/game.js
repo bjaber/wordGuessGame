51ba40af406e2342383a9689550e5e92486b0cdb
Basem Jaber <basemjaber32@gmail.com>
2019-09-01 13:33:47 -0700

    final push for basic javascript homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,31 @@
+
 var wins = 0;
-var losses =0;
-var ties = 0;
+var losses = 0;
+var attempts = 10;
+addEventListener("keyup", myFunction);
+function myFunction() {
+    let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    alert("you pressed " + userGuess);
 
-addEventListener("keyup",myFunction);
-function myFunction(){
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    alert("computer guessed " + computerGuess);
+    if (userGuess === computerGuess) {
+        wins++;
+        alert("you win! Nice work!")
+    }
+    else {
+            attempts--;
+            losses++;
+            alert("ooppss! Try again homie!")
+        }
+        var winsBaby = document.getElementById("winsBaby");
+        var takenLs = document.getElementById("takenLs");
+        var cracksAtIt = document.getElementById("cracksAtIt");
 
-alert("you pressed " + userGuess);
-
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
-    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-var computerGuess = alphabet[Math.floor(Math.random()*alphabet.length)];
-
-alert("computer guessed "+ computerGuess);
-if (userGuess === computerGuess){
-
-    alert("you win! Nice work! ")
-    wins++;
-}
-
-else{
-
-    alert("ooppss! Try again homie!")
-    losses++;
-}
-};
+        winsBaby.textContent = "winsBaby: " + wins;
+        takenLs.textContent = "takenLs: " + losses;
+        cracksAtIt.textContent = "cracksAtIt: " + attempts;
+    };
